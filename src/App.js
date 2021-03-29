ec4a1d2d8889ff3f060c6f02a1cdbbc8c01dd0dc
BAEK SANGHEUN <170410_@naver.com>
2021-03-29 23:12:37 +0900

    # 기존에 장바구니에 있던 제품도 장바구니 버튼을 누르면 수량 추가

--- src/App.js
+++ src/App.js
@@ -9,17 +9,33 @@ function App() {
   const [cartList, setCartList] = useState(dummy.cart);
 
   const addToCart = (id) => {
-    const [addedProduct] = products.filter((product) => product.id === id);
-    for (let i = 0; i < cartList.length; i++) {
-      if (cartList[i].id === addedProduct.id) {
-        console.log("중복", cartList[i].quantity);
-        console.log("중복", cartList[i]);
-        setCartList([{ ...cartList[i], quantity: cartList[i].quantity + 1 }]);
-        return;
-      }
+    // 추가시킬 프로덕트 정보 찾기
+    const productToAdd = products.find((product) => product.id === id);
+
+    // 카트안에 해당 프로덕트가 이미 존재하는지 찾기
+    const existingItem = cartList.find((cartItem) => cartItem.id === id);
+
+    // 카트안에 있었다면
+    if (existingItem) {
+      // 어떻게 업데이트 할지 로직 구현 -> 카트안에 있는 아이템을 갖고
+      const cartToUpdate = cartList.map((cartItem) =>
+        // 추가시킨 아이템의 아이디와 이미 카트안에 있는 물품의 아이디가 같을때
+        cartItem.id === existingItem.id
+          ? {
+              // 카트안에 이미 존재하던 아이템을 풀어주고
+              ...cartItem,
+              // 그 풀어준 값중 quanity의 값을 하나를 더해준다
+              quantity: cartItem.quantity + 1,
+            }
+          : // 카트안에 중복되는 물품이 없다면 그냥 아무작업 하지 않는다
+            cartItem
+      );
+      setCartList(cartToUpdate);
+    } else {
+      // 카트안에 해당 아이템이 없다면 기존 카트아이템에 덧붙여 추가시킬 아이템을 더한다
+      const cartToUpdate = [...cartList, productToAdd];
+      setCartList(cartToUpdate);
     }
-    console.log(cartList);
-    setCartList([...cartList, addedProduct]);
   };
 
   const onIncrease = (id) => {
